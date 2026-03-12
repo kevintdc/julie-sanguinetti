397becb7a6e9013fbedaacba133812d4b6732d7d
Kevin Toudic <toudickevin@gmail.com>
2026-03-12 20:58:32 +0100

    Correction + sécu4

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,32 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
   default-src 'self';
+
   script-src 'self' 'unsafe-inline' 'unsafe-eval'
     https://www.google.com
     https://www.gstatic.com
     https://www.google.com/recaptcha/
-    https://www.gstatic.com/recaptcha/;
+    https://www.gstatic.com/recaptcha/
+    https://www.recaptcha.net;
+
   frame-src 'self'
     https://www.google.com
+    https://www.google.com/recaptcha/
     https://recaptcha.google.com
     https://maps.google.com
-    https://www.google.com/recaptcha/;
+    https://www.recaptcha.net;
+
   connect-src 'self'
     https://www.google.com
     https://www.gstatic.com
     https://maps.googleapis.com
-    https://www.google.com/recaptcha/;
+    https://maps.gstatic.com
+    https://www.google.com/recaptcha/
+    https://www.recaptcha.net;
+
   img-src 'self' data: blob: https:;
   style-src 'self' 'unsafe-inline';
-  font-src 'self' data:;
+  font-src 'self' data: https:;
 `
   .replace(/\s{2,}/g, " ")
   .trim();
